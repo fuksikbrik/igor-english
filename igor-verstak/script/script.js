36b8c2a886a9a84565f28a199ab9d3aae2746ad1
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2023-01-16 15:00:21 +0200

    js testing classs

---
+++ igor-verstak/script/script.js
@@ -0,0 +1,24 @@
+/*const card = document.querySelectorAll("[data-card-word='card']");
+let question = document.querySelector('#question');
+const button = document.querySelector('#next');
+const lvl = document.querySelectorAll('.complexity-icon-lvl');
+
+lvl.forEach(function (item) {
+    item.addEventListener('click', function () {
+     // item.classList.toggle('active');
+      item.classList.toogle('complexity-icon-lvl');
+    })
+});
+
+
+card.forEach(function (item) {
+   item.addEventListener('click', function () {
+    item.classList.toggle('active');
+    item.classList.toggle('words-card');
+   })
+});
+
+button.addEventListener('click', function(){
+        console.log(item);
+
+})*/
