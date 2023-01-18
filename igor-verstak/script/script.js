56510f9cbea5293c0d72603d2a464dfebbcdaf01
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2023-01-18 20:41:42 +0200

    tesing branch problem js active class not active problems in innerHTML

--- igor-verstak/script/script.js
+++ igor-verstak/script/script.js
@@ -1,15 +1,23 @@
-/*const card = document.querySelectorAll("[data-card-word='card']");
+import jsonItem from '../items_a1.json' assert {type: 'json'};
+const card = document.querySelectorAll("[data-card-word='card']");
 let question = document.querySelector('#question');
 const button = document.querySelector('#next');
 const lvl = document.querySelectorAll('.complexity-icon-lvl');
+const words = document. querySelector('.words'); 
+
+
 
 lvl.forEach(function (item) {
     item.addEventListener('click', function () {
      // item.classList.toggle('active');
       item.classList.toogle('complexity-icon-lvl');
     })
-});
+})
 
+words.innerHTML = `<div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[0]}</p></div>
+                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[1]}</p></div>
+                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[2]}</p></div>
+                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[3]}</p></div>`
 
 card.forEach(function (item) {
    item.addEventListener('click', function () {
@@ -19,6 +27,6 @@ card.forEach(function (item) {
 });
 
 button.addEventListener('click', function(){
-        console.log(item);
+        
 
-})*/
+});
