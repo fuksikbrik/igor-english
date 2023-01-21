0c9763963969da0c922a57ec50a1fb805aa4b788
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2023-01-21 11:29:53 +0200

    test commit button active class words button active class level alert true/false

--- igor-verstak/script/script.js
+++ igor-verstak/script/script.js
@@ -1,32 +1,112 @@
 import jsonItem from '../items_a1.json' assert {type: 'json'};
-const card = document.querySelectorAll("[data-card-word='card']");
+const cardOne = document.getElementById('card-01');
+const cardThwo = document.getElementById('card-02');
+const cardThree = document.getElementById('card-03'); 
+const cardFour = document.getElementById('card-04');
 let question = document.querySelector('#question');
+const card = document.querySelectorAll('[data-card-word="card"]')
 const button = document.querySelector('#next');
-const lvl = document.querySelectorAll('.complexity-icon-lvl');
-const words = document. querySelector('.words'); 
+const level = document.querySelectorAll('.complexity-icon-lvl');
+
+let lastWord = card[0];
+let lastLevel = level[0];
+
+cardOne.innerHTML = `<p>${jsonItem.words[0].translations[0]}</p>`;
+cardThwo.innerHTML =`<p>${jsonItem.words[0].translations[1]}</p>`;
+cardThree.innerHTML =`<p>${jsonItem.words[0].translations[2]}</p>`;
+cardFour.innerHTML =`<p>${jsonItem.words[0].translations[3]}</p>`;
+question.innerHTML = `<p>${jsonItem.words[0].ru}</p>` 
+
+for(let i = 0; i < level.length; i++ ){
+    level[i].addEventListener('click', function () {
+        lastLevel.classList.remove('active-lvl');
+        this.classList.add('active-lvl');
+        lastLevel = this
+    });
+}
 
 
 
-lvl.forEach(function (item) {
-    item.addEventListener('click', function () {
-     // item.classList.toggle('active');
-      item.classList.toogle('complexity-icon-lvl');
-    })
-})
+for(let i = 0; i < card.length; i++ ){
 
-words.innerHTML = `<div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[0]}</p></div>
-                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[1]}</p></div>
-                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[2]}</p></div>
-                   <div data-card-word="card" class="words-card"><p>${jsonItem.words[0].translations[3]}</p></div>`
+  card[i].addEventListener('click', function () {
+    lastWord.classList.remove('active');
+    this.classList.add('active');
+    lastWord = this; 
+  });
+}
 
-card.forEach(function (item) {
-   item.addEventListener('click', function () {
-    item.classList.toggle('active');
-    item.classList.toggle('words-card');
-   })
-});
+
+
 
 button.addEventListener('click', function(){
-        
-
+ if(cardOne.classList.contains('active')){
+    alert('Ответ верный!');
+    location. reload()
+  }else{
+    cardOne.classList.add('active');
+    alert('Ответ не верный!');
+    location. reload()
+  }
 });
+
+
+
+
+
+
+
+
+//заметки---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+/*
+card.forEach(function (item) {
+  item.addEventListener('click', function () {
+    if(card.forEach(function (item) { item.addEventListener('click', function () { item.classList.contains('active')} )}) === 1){
+      item.classList.toggle('active');
+      item.classList.toggle('words-card');
+    }
+  })
+});
+*/
+
+
+
+/*
+
+*/
+
+
+/*
+let cells = document.getElementsByClassName('words-card');
+
+for(let i=0; i<cells.length;i++){
+    cells[i].addEventListener('click', selectDate);    
+}
+function selectDate(){
+    this.className += ' active';
+}
+*/
+
+
+
+
+
+
+
+/*
+for (i = 0; words.length > i; i++) {
+  panels[i].onclick = function() {
+    let currentActive = active[0];
+    if (currentActive){
+      currentActive.classList.remove('active');
+    }if (currentActive !== this){
+      this.classList.add('active');
+    }
+  };
+};
+
+*/
